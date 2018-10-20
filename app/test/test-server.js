bb4a21c10269ff682e83364524125aee73490ea8
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-20 12:45:13 +0200

    Setup the first HTTP integration test; server sending static files to client

--- app/test/test-server.js
+++ app/test/test-server.js
@@ -1,6 +1,23 @@
 'use strict';
 
-// Import 3rd party frameworks and project modules
+// Import 3rd party frameworks, libraries or project modules
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server');
+
+// Setup the expect assertion library
+const expect = chai.expect;
+
+// Setup chai HTTP layer testing middleware
+chai.use(chaiHttp);
+
+// Test if the server is sending static files to the client
+describe('main page (index.html)', function() {
+  it('should exist', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
