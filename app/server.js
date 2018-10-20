a1ea08cc8200e7a39ac2f2809051037ec971a9fa
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-20 12:14:06 +0200

    Setup logging of the HTTP layer

--- app/server.js
+++ app/server.js
@@ -2,7 +2,10 @@
 
 // Import 3rd party frameworks
 const express = require('express');
-
+const morgan = require('morgan');
 
 // Initialize an express app
 const app = express();
+
+// Setup logging of the HTTP layer
+app.use(morgan('combined'));
