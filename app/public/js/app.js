b12a844f6805e7196ea1f1e706a318926cba5ea6
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-23 15:19:03 +0200

    Implement close menu when click anywhere on screen

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -9,11 +9,26 @@ function registerEventListeners() {
 }
 
 function profileMenuListener() {
-  $('body').on('click', '.js-avatar', () => {
+  $('.js-avatar').on('click', function(event) {
+    event.stopPropagation();
     toggleProfileMenu();
+  });
+
+  $('body').on('click', () => {
+    if (profileMenuOpen()) {
+      closeProfileMenu();
+    }
   });
 }
 
 function toggleProfileMenu() {
   $('.js-profile-options').toggleClass('profile-options--toggle-visibility');
+}
+
+function profileMenuOpen() {
+  return $('.js-profile-options').hasClass('profile-options--toggle-visibility');
+}
+
+function closeProfileMenu() {
+  $('.js-profile-options').removeClass('profile-options--toggle-visibility');
 }
