2a7a462815cde53c67a81bb8f2f645adc7170fa0
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-31 12:36:33 +0100

    Implement the /api/users/ DELETE endpoint

--- app/user/user.router.js
+++ app/user/user.router.js
@@ -121,4 +121,20 @@ router.put('/:id', (req, res) => {
   });
 });
 
+// DELETE enpoint (JWT protected)
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then(() => {
+      return res.status(HTTP_STATUS_CODES.NO_CONTENT).end();
+    })
+    .catch(err => {
+      res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({
+        code: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error'
+      })
+    });
+});
+
 module.exports = {router};
