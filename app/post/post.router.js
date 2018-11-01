86779d385660ceff861e8cd4e36d02a5aca5629a
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-01 12:53:57 +0100

    Implement jwt auth fro /api/posts/ PUT endpoint

--- app/post/post.router.js
+++ app/post/post.router.js
@@ -128,7 +128,7 @@ router.get('/:id', (req, res) => {
 
 // Update a note (JWT protected)
 
-router.put('/:id', (req, res) => {
+router.put('/:id', jwtAuth, (req, res) => {
   if (!(req.params.id === req.body.id)) {
     return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({
       code: HTTP_STATUS_CODES.BAD_REQUEST,
