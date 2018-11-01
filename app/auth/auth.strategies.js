200fd9dafd7d02860b4fda8e7a622d0d5138520e
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-01 15:16:16 +0100

    Fix import of User model to authentication strategies module

--- app/auth/auth.strategies.js
+++ app/auth/auth.strategies.js
@@ -4,13 +4,13 @@ const passport = require('passport');
 const {Strategy: LocalStrategy} = require('passport-local');
 const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
 
-const {User} = require('../user');
+const {User} = require('../user/user.model');
 const {JWT_SECRET} = require('../config');
 
 const localStrategy = new LocalStrategy((username, password, callback) => {
   let user; 
   
-  User.findOne({username: username})
+  User.findOne({username})
     .then(_user => {
       user = _user;
 
