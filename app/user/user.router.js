edd87cee53515f17abd70d9da487cd616560d4c3
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-01 13:01:52 +0100

    Implement jwt auth for /api/users/ PUT endpoint

--- app/user/user.router.js
+++ app/user/user.router.js
@@ -95,7 +95,7 @@ router.get('/:id', jwtAuth, (req, res) => {
 
 // PUT endpoint (JWT protected)
 
-router.put('/:id', (req, res) => {
+router.put('/:id', jwtAuth, (req, res) => {
   if (!(req.params.id === req.body.id)) {
     return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({
       code: HTTP_STATUS_CODES.BAD_REQUEST,
