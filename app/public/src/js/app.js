dab7686a4daf6db335cb4630c630b6e272e0a9db
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-19 12:40:44 +0100

    Install webpack and setup config file

--- app/public/src/js/app.js
+++ app/public/src/js/app.js
@@ -1,34 +1,38 @@
-$(startApp);
+// $(startApp);
 
-function startApp() {
-  registerEventListeners();
-}
+// function startApp() {
+//   registerEventListeners();
+// }
 
-function registerEventListeners() {
-  profileMenuListener();
-}
+// function registerEventListeners() {
+//   profileMenuListener();
+// }
 
-function profileMenuListener() {
-  $('.js-avatar').on('click', function(event) {
-    event.stopPropagation();
-    toggleProfileMenu();
-  });
+// function profileMenuListener() {
+//   $('.js-avatar').on('click', function(event) {
+//     event.stopPropagation();
+//     toggleProfileMenu();
+//   });
 
-  $('body').on('click', () => {
-    if (isProfileMenuOpen()) {
-      closeProfileMenu();
-    }
-  });
-}
+//   $('body').on('click', () => {
+//     if (isProfileMenuOpen()) {
+//       closeProfileMenu();
+//     }
+//   });
+// }
 
-function toggleProfileMenu() {
-  $('.js-profile-options').toggleClass('profile-options--toggle-visibility');
-}
+// function toggleProfileMenu() {
+//   $('.js-profile-options').toggleClass('profile-options--toggle-visibility');
+// }
 
-function isProfileMenuOpen() {
-  return $('.js-profile-options').hasClass('profile-options--toggle-visibility');
-}
+// function isProfileMenuOpen() {
+//   return $('.js-profile-options').hasClass('profile-options--toggle-visibility');
+// }
 
-function closeProfileMenu() {
-  $('.js-profile-options').removeClass('profile-options--toggle-visibility');
-}
+// function closeProfileMenu() {
+//   $('.js-profile-options').removeClass('profile-options--toggle-visibility');
+// }
+
+import string from './test';
+
+console.log(string());
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/public/src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, './app/public/dist'),
+    filename: 'js/bundle.js'
+  },
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, './app/public/dist'),
+  //   historyApiFallback: true,
+  //   hot: true,
+  //   inline: true,
+  
+  //   host: 'localhost', // Defaults to `localhost`
+  //   port: 3000, // Defaults to 8080
+  //   proxy: {
+  //     '^/api/*': {
+  //       target: 'http://localhost:8080/api/',
+  //       secure: false
+  //     }
+  //   }
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/public/src/index.html'
+    })
+    // new webpack.HotModuleReplacementPlugin({
+    //   multiStep: true
+    // })
+  ]
+}
